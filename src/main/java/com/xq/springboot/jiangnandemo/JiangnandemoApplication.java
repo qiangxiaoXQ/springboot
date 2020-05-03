a65afef07c0ef8d0341b8dbefa67531a1a785692
XQ <295953863@qq.com>
2020-05-03 16:03:24 +0800

    new create

---
+++ src/main/java/com/xq/springboot/jiangnandemo/JiangnandemoApplication.java
@@ -0,0 +1,13 @@
+package com.xq.springboot.jiangnandemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiangnandemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiangnandemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xq/springboot/jiangnandemo/JiangnandemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xq.springboot.jiangnandemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiangnandemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
